5d1bfc293d1832853cbead7d9380eaf374f18468
AngelLedesma123 <87627845+AngelLedesma123@users.noreply.github.com>
2021-07-23 08:58:27 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyAdmWWV6HV8sI-MfxijmgdUwh8VOeNkMC0",
+    authDomain: "jorge-rivera-autentificacion.firebaseapp.com",
+    projectId: "jorge-rivera-autentificacion",
+    storageBucket: "jorge-rivera-autentificacion.appspot.com",
+    messagingSenderId: "324262565668",
+    appId: "1:324262565668:web:f309fbd3266467982845b1",
+    measurementId: "G-2R4YXE50XX"
+});
